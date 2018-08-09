277e3b0ee2010ce68c7746a71fb7e94845868cbc
Calvin Behling <calvin.behling@gmail.com>
2018-08-09 17:15:48 -0500

    add script to write iso to usb

--- livecd/00-download.sh
+++ livecd/00-download.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-ISO_FILE='debian-live-9.5.0-amd64-xfce.iso'
-SHA512='d2b0fe266ef809943f24b71f6a7c1c7a1645a2ee0b51e71bb05625ca84f16904a8889287d580fcb5d8ff1152785f90ffdb1621733bbb148431155478b0006114'
-
-wget -c "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/${ISO_FILE}"
+set -euo pipefail
+wget -c "${ISO_URL}" --output-document="${ISO_FILE}"
 sha512sum --check --strict <<< "${SHA512}  ${ISO_FILE}"
 
---
+++ livecd/config.sh
@@ -0,0 +1,4 @@
+ISO_FILE='debian-live-9.5.0-amd64-xfce.iso'
+ISO_URL="https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/${ISO_FILE}"
+ISO_SHA512='d2b0fe266ef809943f24b71f6a7c1c7a1645a2ee0b51e71bb05625ca84f16904a8889287d580fcb5d8ff1152785f90ffdb1621733bbb148431155478b0006114'
+
